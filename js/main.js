a1e5b1d89dc2002d44001e4efb9e6cea7ecaff0f
Sanjee <sach0017@student.monash.edu>
2026-05-19 16:06:28 +1000

    Complete first full draft of endangered wildlife scrollytelling webpage

--- js/main.js
+++ js/main.js
@@ -5,3 +5,15 @@ vegaEmbed('#streamgraph-vis', 'charts/streamgraph.vg.json');
 vegaEmbed('#species-ranking-vis', 'charts/species-ranking.vg.json');
 
 vegaEmbed('#sankey-vis', 'charts/sankey.vg.json');
+
+vegaEmbed('#scatter-vis', 'charts/bushfire-scatter.vg.json');
+
+vegaEmbed('#treemap-vis', 'charts/treemap.vg.json');
+
+vegaEmbed('#heatmap-vis', 'charts/heatmap.vg.json');
+
+vegaEmbed('#radial-vis', 'charts/radial-funding.vg.json');
+
+vegaEmbed('#ridgeline-vis', 'charts/ridgeline.vg.json');
+
+vegaEmbed('#timeline-vis', 'charts/timeline.vg.json');
